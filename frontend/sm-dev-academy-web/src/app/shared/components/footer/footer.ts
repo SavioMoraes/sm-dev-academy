ffe597a3e0cc4a7865d1e6d2394220106dc08127
SavioMoraes <smmsavio@hotmail.com>
2026-05-16 18:58:03 -0300

    feat(footer): create reusable navigation footer component

--- frontend/sm-dev-academy-web/src/app/shared/components/footer/footer.ts
+++ frontend/sm-dev-academy-web/src/app/shared/components/footer/footer.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { footerItems } from '../../../core/navigation/footer-items';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  footerItems = footerItems;
+}
