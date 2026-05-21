7f24c65043eb0d46c3428161768849282c05ba90
SavioMoraes <smmsavio@hotmail.com>
2026-05-20 21:40:20 -0300

    feat(header): improve responsive search experience across mobile and desktop

--- frontend/sm-dev-academy-web/src/app/shared/components/header/header.ts
+++ frontend/sm-dev-academy-web/src/app/shared/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { navigationItems } from '../../../core/navigation/navigation-items';
 import { categoryItems } from '../../../core/navigation/category-items';
 import { Footer } from '../footer/footer';
@@ -12,13 +12,22 @@ import { Footer } from '../footer/footer';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnInit {
 
   isMobileMenuOpen = false;
   isSearchActive = false;
+  isMobileOrTablet = false;
 
   navigationItems = navigationItems;
   categoryItems = categoryItems;
+
+  ngOnInit(): void {
+    this.checkViewport();
+  }
+
+  checkViewport(): void {
+    this.isMobileOrTablet = window.innerWidth <= 1023;
+  }
 
   toggleMobileMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
@@ -28,11 +37,19 @@ export class Header {
     this.isSearchActive = !this.isSearchActive;
   }
 
+  @HostListener('window:resize')
+  onResize(): void {
+    this.checkViewport();
+  }
+
   @HostListener('document:click', ['$event'])
   handleClickOutside(event: MouseEvent): void {
     const target = event.target as HTMLElement;
 
-    if (!target.closest('.header__search')) {
+    if (
+      !target.closest('.header__search') &&
+      !target.closest('.header-search-mobile')
+    ) {
       this.isSearchActive = false;
     }
   }
