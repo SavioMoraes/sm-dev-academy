0468f6393e04a72fa975fec572b5f51bc546b299
SavioMoraes <smmsavio@hotmail.com>
2026-05-22 01:28:04 -0300

    refactor(app-structure): migrate pages to feature-based architecture

--- frontend/sm-dev-academy-web/src/app/app.routes.ts
+++ frontend/sm-dev-academy-web/src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () =>
-          import('./pages/home/home')
+          import('./features/home/home')
             .then((m) => m.Home),
       },
 
@@ -25,28 +25,28 @@ export const routes: Routes = [
       {
         path: 'biblioteca/cursos',
         loadComponent: () =>
-          import('./pages/biblioteca/cursos/cursos')
+          import('./features/biblioteca/cursos/cursos')
             .then((m) => m.Cursos),
       },
 
       {
         path: 'biblioteca/meus-cursos',
         loadComponent: () =>
-          import('./pages/biblioteca/meus-cursos/meus-cursos')
+          import('./features/biblioteca/meus-cursos/meus-cursos')
             .then((m) => m.MeusCursos),
       },
 
       {
         path: 'biblioteca/favoritos',
         loadComponent: () =>
-          import('./pages/biblioteca/favoritos/favoritos')
+          import('./features/biblioteca/favoritos/favoritos')
             .then((m) => m.Favoritos),
       },
 
       {
         path: 'biblioteca/historico',
         loadComponent: () =>
-          import('./pages/biblioteca/historico/historico')
+          import('./features/biblioteca/historico/historico')
             .then((m) => m.Historico),
       },
 
@@ -57,84 +57,84 @@ export const routes: Routes = [
       {
         path: 'tecnologias/html',
         loadComponent: () =>
-          import('./pages/tecnologias/html/html')
+          import('./features/tecnologias/html/html')
             .then((m) => m.Html),
       },
 
       {
         path: 'tecnologias/css',
         loadComponent: () =>
-          import('./pages/tecnologias/css/css')
+          import('./features/tecnologias/css/css')
             .then((m) => m.Css),
       },
 
       {
         path: 'tecnologias/javascript',
         loadComponent: () =>
-          import('./pages/tecnologias/javascript/javascript')
+          import('./features/tecnologias/javascript/javascript')
             .then((m) => m.Javascript),
       },
 
       {
         path: 'tecnologias/typescript',
         loadComponent: () =>
-          import('./pages/tecnologias/typescript/typescript')
+          import('./features/tecnologias/typescript/typescript')
             .then((m) => m.Typescript),
       },
 
       {
         path: 'tecnologias/angular',
         loadComponent: () =>
-          import('./pages/tecnologias/angular/angular')
+          import('./features/tecnologias/angular/angular')
             .then((m) => m.Angular),
       },
 
       {
         path: 'tecnologias/react',
         loadComponent: () =>
-          import('./pages/tecnologias/react/react')
+          import('./features/tecnologias/react/react')
             .then((m) => m.React),
       },
 
       {
         path: 'tecnologias/python',
         loadComponent: () =>
-          import('./pages/tecnologias/python/python')
+          import('./features/tecnologias/python/python')
             .then((m) => m.Python),
       },
 
       {
         path: 'tecnologias/nodejs',
         loadComponent: () =>
-          import('./pages/tecnologias/nodejs/nodejs')
+          import('./features/tecnologias/nodejs/nodejs')
             .then((m) => m.Nodejs),
       },
 
       {
         path: 'tecnologias/nestjs',
         loadComponent: () =>
-          import('./pages/tecnologias/nestjs/nestjs')
+          import('./features/tecnologias/nestjs/nestjs')
             .then((m) => m.Nestjs),
       },
 
       {
         path: 'tecnologias/mongodb',
         loadComponent: () =>
-          import('./pages/tecnologias/mongodb/mongodb')
+          import('./features/tecnologias/mongodb/mongodb')
             .then((m) => m.Mongodb),
       },
 
       {
         path: 'tecnologias/mysql',
         loadComponent: () =>
-          import('./pages/tecnologias/mysql/mysql')
+          import('./features/tecnologias/mysql/mysql')
             .then((m) => m.Mysql),
       },
 
       {
         path: 'tecnologias/postgresql',
         loadComponent: () =>
-          import('./pages/tecnologias/postgresql/postgresql')
+          import('./features/tecnologias/postgresql/postgresql')
             .then((m) => m.Postgresql),
       },
 
@@ -145,7 +145,7 @@ export const routes: Routes = [
       {
         path: 'conta/perfil',
         loadComponent: () =>
-          import('./pages/conta/perfil/perfil')
+          import('./features/conta/perfil/perfil')
             .then((m) => m.Perfil),
       },
 
@@ -156,7 +156,7 @@ export const routes: Routes = [
       {
         path: 'admin/dashboard',
         loadComponent: () =>
-          import('./pages/admin/dashboard/dashboard')
+          import('./features/admin/dashboard/dashboard')
             .then((m) => m.Dashboard),
       },
     ],
