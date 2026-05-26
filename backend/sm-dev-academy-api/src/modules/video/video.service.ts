d21c3c852792682cfcde2e8eaab5cd7c218720c1
SavioMoraes <smmsavio@hotmail.com>
2026-05-26 16:58:32 -0300

    feat(video): integrate youtube api with nestjs video module

---
+++ backend/sm-dev-academy-api/src/modules/video/video.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class VideoService {
+
+  async searchVideos(search: string) {
+
+    const response = await axios.get(
+      'https://www.googleapis.com/youtube/v3/search',
+      {
+        params: {
+          key: process.env.YOUTUBE_API_KEY,
+          part: 'snippet',
+          q: search,
+          type: 'video',
+          maxResults: 12,
+        },
+      },
+    );
+
+    return response.data.items;
+  }
+
+}
