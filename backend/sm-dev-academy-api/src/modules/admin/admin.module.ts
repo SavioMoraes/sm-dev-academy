36aa57c3945df8e347faa6d9a85061b3172fefaa
SavioMoraes <smmsavio@hotmail.com>
2026-06-04 01:24:44 -0300

    feat(courses): implement infinite scroll for course listing

--- backend/sm-dev-academy-api/src/modules/admin/admin.module.ts
+++ backend/sm-dev-academy-api/src/modules/admin/admin.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-
 import { AuthModule } from '../auth/auth.module';
 import { YoutubeModule } from '../youtube/youtube.module';
-
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { PrismaService } from '../../database/prisma.service';
 
 @Module({
   imports: [
@@ -16,6 +15,7 @@ import { AdminService } from './admin.service';
   ],
   providers: [
     AdminService,
+    PrismaService,
   ],
 })
 export class AdminModule {}
