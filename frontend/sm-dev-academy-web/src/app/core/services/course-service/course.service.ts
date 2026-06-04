d86629d6d7eb94ebd45dc925aca47432e306e9b3
SavioMoraes <smmsavio@hotmail.com>
2026-06-04 02:30:04 -0300

    teste mobile

--- frontend/sm-dev-academy-web/src/app/core/services/course-service/course.service.ts
+++ frontend/sm-dev-academy-web/src/app/core/services/course-service/course.service.ts
@@ -2,25 +2,19 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CoursesResponse } from '../../interfaces/course.interface';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
 
-  private readonly http =
-    inject(HttpClient);
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/learn/courses`;
 
-  private readonly apiUrl =
-    'http://localhost:3000/learn/courses';
-
-  getCourses():
-    Observable<CoursesResponse> {
-
+  getCourses(): Observable<CoursesResponse> {
     return this.http.get<CoursesResponse>(
       this.apiUrl,
     );
-
   }
-
 }
