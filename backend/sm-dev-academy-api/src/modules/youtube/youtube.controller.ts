912d8cabef433a7ece8172f88b50e3c4b9fbb05f
SavioMoraes <smmsavio@hotmail.com>
2026-06-30 19:05:18 -0300

    refactor(admin): optimize YouTube course import workflow

--- backend/sm-dev-academy-api/src/modules/youtube/youtube.controller.ts
+++ backend/sm-dev-academy-api/src/modules/youtube/youtube.controller.ts
@@ -4,9 +4,4 @@ import { YoutubeService } from './youtube.service';
 @Controller('learn')
 export class YoutubeController {
   constructor(private readonly youtubeService: YoutubeService) {}
-
-  @Get('courses')
-  async getCourses() {
-    return this.youtubeService.getCourses([]);
-  }
 }
